84bab8c24ea190f4420923b60ba3e48437f8db8b
Nicci Sheets <niccisheets@aol.com>
2018-07-15 01:11:38 -0400

    modified my coin_arr testing

--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -4,8 +4,8 @@ require_relative "coin_changer.rb"
 class TestCoinChanger < Minitest::Test
 
 	def test_creating_array_with_coin_values
-		coins = 25, 10, 5, 1
-		assert_equal([25, 10, 5, 1], coin_arr(coins))
+		coins = 10, 25, 5, 1
+		assert_equal(coins, coin_arr(coins))
 		assert_equal(Array, coin_arr(coins).class)
 		assert_equal(4, coin_arr(coins).size)
 	end
