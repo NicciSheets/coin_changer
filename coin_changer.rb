68514d3f15db31bae861c77949c9f2d24b45d7aa
Nicci Sheets <niccisheets@aol.com>
2018-07-16 11:09:02 -0400

    saving final copy

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,63 +1,8 @@
-
+require 'pp'
 
 def coin_arr(coins)
 	coins.sort! { |x, y| y <=> x }
 end
-
-def coin_name_arr(coin_name)
-	coin_name
-end
-
-# def coin_hash(coin_with_amount)
-# 	coin_with_amount
-# end
-
-###  don't need previous 3 definitions for the actual function for the coin)_changer.  Only included them to swork on tdd.
-	
-# def coin_changer(money) 
-# 	change = {}
-# 	while money > 0 do
-# 		if money >= 25
-# 			money -= 25
-# 			if change.key?(:quarter) then change[:quarter] += 1
-# 			else
-# 				change[:quarter] = 1
-# 			end
-# 		elsif money >= 10
-# 			money -= 10
-# 			if change.key?(:dime) then change[:dime] += 1
-# 			else
-# 				change[:dime] = 1
-# 			end
-# 		elsif money >= 5
-# 		   money -= 5		
-# 		   if change.key?(:nickel) then change[:nickel] += 1
-# 			else
-# 				change[:nickel] = 1
-# 			end
-# 		elsif money >= 1
-# 			money -= 1
-# 			if change.key?(:penny) then change[:penny] += 1
-# 			else
-# 				change[:penny] = 1
-# 			end
-# 		end
-# 	end
-# 	change
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def coin_change(money, coins)
 	change = {}
@@ -72,6 +17,6 @@ end
 # money = 67
 # coin_name = ["quarter", "dime", "nickel", "penny"]
 # coins = [25, 10, 5, 1]
-# #coin_change(money, coins)
-# p coin_hash(money, coins)
+# pp coin_change(money, coins)
+# pp coin_hash(money, coins)
 
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -11,26 +11,17 @@ class TestCoinChanger < Minitest::Test
 		assert_equal([25, 10, 5, 1], coin_arr(coins))
 	end
 
-	def test_creating_array_with_coin_names
-		coin_names = "quarter", "dime", "nickel", "penny"
-		assert_equal(coin_names, coin_name_arr(coin_names))
-		assert_equal(Array, coin_name_arr(coin_names).class)
-		assert_equal(4, coin_name_arr(coin_names).size)
-		assert_equal(["quarter", "dime", "nickel", "penny"], coin_name_arr(coin_names))
+	def test_coin_change_array
+		money = 67
+		coins = 10, 25, 5, 1
+		assert_equal([[25, 25], [10], [5], [1, 1]], coin_change(money, coins))
 	end
 
-	# def test_assert_building_hash_with_coin_values_and_names
-	# 	coin_with_amount = {quarter: 25, dime: 10, nickel: 5, penny: 1}
-	# 	assert_equal(coin_with_amount, coin_hash(coin_with_amount))
-	# 	assert_equal(Hash, coin_hash(coin_with_amount).class)
-	# 	assert_equal(25, coin_hash(coin_with_amount)[:quarter])
-	# 	assert_equal(10, coin_hash(coin_with_amount)[:dime])
-	# 	assert_equal(5, coin_hash(coin_with_amount)[:nickel])
-	# 	assert_equal(1, coin_hash(coin_with_amount)[:penny])
-	# end
-	
-	### previous 3 tests are not used in the final coin_changer app, but were merely dont for tdd learning purposes
-
+	def test_coin_change_array
+		money = 123
+		coins = 10, 25, 5, 1
+		assert_equal([[25, 25, 25, 25], [10, 10], [], [1, 1, 1]], coin_change(money, coins))
+	end
 	def test_coin_changer_returns_every_key
 		money = 67
 		coins = 10, 25, 5, 1
@@ -38,6 +29,10 @@ class TestCoinChanger < Minitest::Test
 	end
 
 
-
+	def test_coin_changer_returns_every_key
+		money = 123
+		coins = 10, 25, 5, 1
+		assert_equal({quarter: 4, dime: 2, nickel: 0, penny: 3}, coin_hash(money, coins))
+	end
 
 end
