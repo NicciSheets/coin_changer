a4f6f300a412743d72bd2886c73ebd5893f0ba45
Nicci Sheets <niccisheets@aol.com>
2018-07-15 20:55:41 -0400

    testing for creating multidimensional arrays which set up for creating hash with coin names

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,15 +1,16 @@
 
 
-def coin_arr(coins)
-	coins.sort! { |x, y| y <=> x }
-	coins
+def coin_arr(coin_val)
+	coin_val.sort! { |x, y| y <=> x }
 end
 
 
-def coin_value_hash(coins)
-	
+def multidimen_arr(coin_val) #creates one array with subarrays that follow pattern ("", coin_val), which allows us to plsg in the coin_name associated with each value in next function.  Doing this separately, in case there's ever a change in the currentcy used for this - kinda thinking head, or trying to.
+	coin_array = coin_arr(coin_val)
+	coin_multidimen_arr= coin_array.collect { |value| ["", value] }
+	coin_multidimen_arr
 end
 
 
-# coins = 25, 10, 5, 1
-# coin_arr(coins)
+#coin_val = 25, 10, 5, 1
+#p multidimen_arr(coin_val)
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -4,26 +4,26 @@ require_relative "coin_changer.rb"
 class TestCoinChanger < Minitest::Test
 
 	def test_creating_array_with_coin_values
-		coins = 10, 25, 5, 1
-		assert_equal(coins, coin_arr(coins))
-		assert_equal(Array, coin_arr(coins).class)
-		assert_equal(4, coin_arr(coins).size)
-		assert_equal([25, 10, 5, 1], coin_arr(coins))
+		coin_val = 10, 25, 5, 1
+		assert_equal(coin_val, coin_arr(coin_val))
+		assert_equal(Array, coin_arr(coin_val).class)
+		assert_equal(4, coin_arr(coin_val).size)
+		assert_equal([25, 10, 5, 1], coin_arr(coin_val))
 	end
 
 	def test_coin_arr_is_sorted_high_to_low
-		coins = 10, 25, 5, 1
-		assert_equal(25, coin_arr(coins)[0])
-		assert_equal(10, coin_arr(coins)[1])
-		assert_equal(5, coin_arr(coins)[2])
-		assert_equal(1, coin_arr(coins)[3])
+		coin_val = 10, 25, 5, 1
+		assert_equal(25, coin_arr(coin_val)[0])
+		assert_equal(10, coin_arr(coin_val)[1])
+		assert_equal(5, coin_arr(coin_val)[2])
+		assert_equal(1, coin_arr(coin_val)[3])
 	end
 
-	def test_creating_hash_from_coin_values
-		coins = 10, 25, 5, 1
-		assert_equal(Hash, coin_value_hash(coins).class)
-		assert_equal(4, coin_value_hash(coins).size)
-		assert_equal({ :quarter => 25, :dime => 10, :nickel => 5, :penny => 1 }, coin_value_hash(coins))
+	def test_creating_multidimensional_array_leading_into_hashes
+		coin_val = 10, 25, 5, 1
+		assert_equal(Array, multidimen_arr(coin_val).class)
+		assert_equal(4, multidimen_arr(coin_val).size)
+		assert_equal([["", 25], ["", 10], ["", 5], ["", 1]], multidimen_arr(coin_val))
 	end
 
 end
