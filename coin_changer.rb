881a71ead9774a6db30fb31cf2c4741809a577eb
Nicci Sheets <niccisheets@aol.com>
2018-07-16 02:57:27 -0400

    still need more tdd for coin changer

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,17 +1,47 @@
 
 
-def coin_arr(coin_val)
-	coin_val.sort! { |x, y| y <=> x }
+def coin_arr(coins)
+	coins.sort! { |x, y| y <=> x }
 end
 
 def coin_name_arr(coin_name)
 	coin_name
 end
-###  don't really need these for the end function used for coin changer, did these just to show testing for the elements within the hash
 
 def coin_hash(coin_with_amount)
 	coin_with_amount
 end
+
+###  don't need previous 3 definitions for the actual function for the coin)_changer.  Only included them to swork on tdd.
 	
-def coin_return(money, coin_val)
+def coin_changer(money) 
+	change = {}
+	while money > 0 do
+		if money >= 25
+			money -= 25
+			if change.key?(:quarter) then change[:quarter] += 1
+			else
+				change[:quarter] = 1
+			end
+		elsif money >= 10
+			money -= 10
+			if change.key?(:dime) then change[:dime] += 1
+			else
+				change[:dime] = 1
+			end
+		elsif money >= 5
+		   money -= 5		
+		   if change.key?(:nickel) then change[:nickel] += 1
+			else
+				change[:nickel] = 1
+			end
+		elsif money >= 1
+			money -= 1
+			if change.key?(:penny) then change[:penny] += 1
+			else
+				change[:penny] = 1
+			end
+		end
+	end
+	change
 end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -4,11 +4,11 @@ require_relative "coin_changer.rb"
 class TestCoinChanger < Minitest::Test
 
 	def test_creating_array_with_coin_values
-		coin_val = 10, 25, 5, 1
-		assert_equal(coin_val, coin_arr(coin_val))
-		assert_equal(Array, coin_arr(coin_val).class)
-		assert_equal(4, coin_arr(coin_val).size)
-		assert_equal([25, 10, 5, 1], coin_arr(coin_val))
+		coins = 10, 25, 5, 1
+		assert_equal(coins, coin_arr(coins))
+		assert_equal(Array, coin_arr(coins).class)
+		assert_equal(4, coin_arr(coins).size)
+		assert_equal([25, 10, 5, 1], coin_arr(coins))
 	end
 
 	def test_creating_array_with_coin_names
@@ -20,7 +20,7 @@ class TestCoinChanger < Minitest::Test
 	end
 
 	def test_assert_building_hash_with_coin_values_and_names
-		coin_with_amount= {quarter: 25, dime: 10, nickel: 5, penny: 1}
+		coin_with_amount = {quarter: 25, dime: 10, nickel: 5, penny: 1}
 		assert_equal(coin_with_amount, coin_hash(coin_with_amount))
 		assert_equal(Hash, coin_hash(coin_with_amount).class)
 		assert_equal(25, coin_hash(coin_with_amount)[:quarter])
@@ -29,4 +29,15 @@ class TestCoinChanger < Minitest::Test
 		assert_equal(1, coin_hash(coin_with_amount)[:penny])
 	end
 	
+	### previous 3 tests are not used in the final coin_changer app, but were merely dont for tdd learning purposes
+
+	def test_coin_changer_returns_every_key
+		money = 67
+		assert_equal({quarter: 2, dime: 1, nickel: 1, penny: 2}, coin_changer(money))
+	end
+
+
+
+
+
 end
