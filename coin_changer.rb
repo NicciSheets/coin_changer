e61185310ab25d2504e38501c9140c0014314f4e
Nicci Sheets <niccisheets@aol.com>
2018-07-16 10:35:07 -0400

    fixed everything

--- coin_changer.rb
+++ coin_changer.rb
@@ -8,40 +8,70 @@ def coin_name_arr(coin_name)
 	coin_name
 end
 
-def coin_hash(coin_with_amount)
-	coin_with_amount
-end
+# def coin_hash(coin_with_amount)
+# 	coin_with_amount
+# end
 
 ###  don't need previous 3 definitions for the actual function for the coin)_changer.  Only included them to swork on tdd.
 	
-def coin_changer(money) 
+# def coin_changer(money) 
+# 	change = {}
+# 	while money > 0 do
+# 		if money >= 25
+# 			money -= 25
+# 			if change.key?(:quarter) then change[:quarter] += 1
+# 			else
+# 				change[:quarter] = 1
+# 			end
+# 		elsif money >= 10
+# 			money -= 10
+# 			if change.key?(:dime) then change[:dime] += 1
+# 			else
+# 				change[:dime] = 1
+# 			end
+# 		elsif money >= 5
+# 		   money -= 5		
+# 		   if change.key?(:nickel) then change[:nickel] += 1
+# 			else
+# 				change[:nickel] = 1
+# 			end
+# 		elsif money >= 1
+# 			money -= 1
+# 			if change.key?(:penny) then change[:penny] += 1
+# 			else
+# 				change[:penny] = 1
+# 			end
+# 		end
+# 	end
+# 	change
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def coin_change(money, coins)
 	change = {}
-	while money > 0 do
-		if money >= 25
-			money -= 25
-			if change.key?(:quarter) then change[:quarter] += 1
-			else
-				change[:quarter] = 1
-			end
-		elsif money >= 10
-			money -= 10
-			if change.key?(:dime) then change[:dime] += 1
-			else
-				change[:dime] = 1
-			end
-		elsif money >= 5
-		   money -= 5		
-		   if change.key?(:nickel) then change[:nickel] += 1
-			else
-				change[:nickel] = 1
-			end
-		elsif money >= 1
-			money -= 1
-			if change.key?(:penny) then change[:penny] += 1
-			else
-				change[:penny] = 1
-			end
-		end
-	end
-	change
+	coin_arr(coins).map { |coin| f = money/coin; money%=coin; Array.new(f){coin} }
 end
+
+def coin_hash(money, coins)
+	coin_change_flat = coin_change(money, coins).flatten
+	coin_hash = { quarter: coin_change_flat.count(25), dime: coin_change_flat.count(10), nickel: coin_change_flat.count(5), penny: coin_change_flat.count(1) }
+end
+
+# money = 67
+# coin_name = ["quarter", "dime", "nickel", "penny"]
+# coins = [25, 10, 5, 1]
+# #coin_change(money, coins)
+# p coin_hash(money, coins)
+
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -19,23 +19,23 @@ class TestCoinChanger < Minitest::Test
 		assert_equal(["quarter", "dime", "nickel", "penny"], coin_name_arr(coin_names))
 	end
 
-	def test_assert_building_hash_with_coin_values_and_names
-		coin_with_amount = {quarter: 25, dime: 10, nickel: 5, penny: 1}
-		assert_equal(coin_with_amount, coin_hash(coin_with_amount))
-		assert_equal(Hash, coin_hash(coin_with_amount).class)
-		assert_equal(25, coin_hash(coin_with_amount)[:quarter])
-		assert_equal(10, coin_hash(coin_with_amount)[:dime])
-		assert_equal(5, coin_hash(coin_with_amount)[:nickel])
-		assert_equal(1, coin_hash(coin_with_amount)[:penny])
-	end
+	# def test_assert_building_hash_with_coin_values_and_names
+	# 	coin_with_amount = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+	# 	assert_equal(coin_with_amount, coin_hash(coin_with_amount))
+	# 	assert_equal(Hash, coin_hash(coin_with_amount).class)
+	# 	assert_equal(25, coin_hash(coin_with_amount)[:quarter])
+	# 	assert_equal(10, coin_hash(coin_with_amount)[:dime])
+	# 	assert_equal(5, coin_hash(coin_with_amount)[:nickel])
+	# 	assert_equal(1, coin_hash(coin_with_amount)[:penny])
+	# end
 	
 	### previous 3 tests are not used in the final coin_changer app, but were merely dont for tdd learning purposes
 
 	def test_coin_changer_returns_every_key
 		money = 67
-		assert_equal({quarter: 2, dime: 1, nickel: 1, penny: 2}, coin_changer(money))
+		coins = 10, 25, 5, 1
+		assert_equal({quarter: 2, dime: 1, nickel: 1, penny: 2}, coin_hash(money, coins))
 	end
-
 
 
 
