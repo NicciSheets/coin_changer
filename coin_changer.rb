0b43f8a418098cff9c641d33f874178e662a2143
Nicci Sheets <niccisheets@aol.com>
2018-07-19 19:19:48 -0400

    working on allowing for larger numbers

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,26 +1,30 @@
 
+
 def coin_arr(coins)
-	puts "in coin_arr, coins is #{coins}"
 	coins.sort! { |x, y| y <=> x }
 end
 
 def coin_change(money, coins)
-	puts "in coin_change, money is #{money}"
-	puts "in coin_change, coins is #{coins}"
-	coin_arr(coins).map { |coin| f = money/coin; money%=coin; Array.new(f){coin} }
+	f_arr = []
+	coin_arr(coins).each do |coins| 
+		f = money/coins
+		money%=coins
+		f_arr << f
+	end
+	f_arr
+	p f_arr
 end
+	
 
 def coin_hash(money, coins)
-	puts "in coin_hash money is #{money}"
-	puts "in coin_hash, coins is #{coins}"
-	coin_change_flat = coin_change(money, coins).flatten
-	coin_hash = { quarter: coin_change_flat.count(25), dime: coin_change_flat.count(10), nickel: coin_change_flat.count(5), penny: coin_change_flat.count(1) }
+	coin_change2 = coin_change(money, coins)
+	coin_hash = { quarter: coin_change2[0], dime: coin_change2[1], nickel: coin_change2[2], penny: coin_change2[3] }
 end
 
 
-#money = 123
-#coins = [25, 10, 5, 1]
+money = 123
+coins = [25, 10, 5, 1]
 ##p coin_change(money, coins)
-#p coin_hash(money, coins)
+p coin_hash(money, coins)
 
 #p coin_hash(7657856785758, [10, 25, 5, 1])
